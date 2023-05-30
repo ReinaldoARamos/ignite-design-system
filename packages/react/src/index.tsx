f4a0b18ad92ec1e986951f511e560d55b450734e
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-05-30 15:52:15 -0300

    confiogurando o storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,14 +1,9 @@
 import {styled} from './styles'
 
-const Button = styled('button' , {
+export const Button = styled('button' , {
     fontFamily: '$default',
     backgroundColor: '$gray700',
     borderRadius: '$md',
     padding:  '$4',
     height: '$10'
 })
-export function App() {
-    return (
-        <Button>Hello</Button>
-    )
-}
