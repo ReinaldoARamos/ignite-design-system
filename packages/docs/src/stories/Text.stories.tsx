085416aaddee942dfc1c2d49ce165bf09fcac528
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-01 16:38:42 -0300

    criando componente Text

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj} from '@storybook/react'
+import {Text, TextProps} from "@ignite-ui/react"
+
+
+
+export default  {
+    title: 'Typhography/Text',
+    component: Text,
+
+    args: {
+        children: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor inc'
+    }  
+    
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {
+ 
+}
+
+export const CostumTag : StoryObj<TextProps> = {
+ args: {
+    children:'Strong Text',
+    as: 'strong'
+ }
+ 
+}
