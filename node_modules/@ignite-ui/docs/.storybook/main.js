a4581e4b62cec3191289af3ea7b42de1f5e95bc2
Reinaldo Ramos <Gamersolitavi4l@gmail.com>
2023-06-05 17:18:45 -0300

    configurando subpath do storybook na produçãoo

--- node_modules/@ignite-ui/docs/.storybook/main.js
+++ node_modules/@ignite-ui/docs/.storybook/main.js
@@ -13,5 +13,11 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, {configType}) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/ignite-design-system'
+    }
+    return config
+  }
 };
 export default config;
